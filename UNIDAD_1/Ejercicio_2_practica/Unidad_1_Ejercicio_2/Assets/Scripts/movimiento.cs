b1e500e0cd652962590ad103c4dcbb85ca392fc9
Jacobo Bedoya <jacobo.bedoya@upb.edu.co>
2021-01-27 16:44:40 -0500

    Creción de movimineto

---
+++ UNIDAD_1/Ejercicio_2_practica/Unidad_1_Ejercicio_2/Assets/Scripts/movimiento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int velocidad ;
+  
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.back, velocidad * Time.deltaTime);
+    }
+
+    
+}
